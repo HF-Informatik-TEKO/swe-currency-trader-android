2091ed13af02faa98072cf57f93b58a9d7fe560a
NRG-Drink <beatdz93@hotmail.com>
2024-01-23 17:45:53 +0100

    init

---
+++ app/src/main/java/ch/teko/currencytrader/Title.java
@@ -0,0 +1,14 @@
+package ch.teko.currencytrader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Title extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
